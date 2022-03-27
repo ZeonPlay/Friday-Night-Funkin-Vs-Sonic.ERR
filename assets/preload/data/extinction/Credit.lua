1f92c6affe79c6c81f40f5a9be2d4eee58c02164
ZeonPlay <95984543+ZeonPlay@users.noreply.github.com>
2022-03-27 09:03:19 +0700

    Add files via upload

--- assets/preload/data/extinction/Credit.lua
+++ assets/preload/data/extinction/Credit.lua
@@ -1,1 +1,5 @@
-
+function onCreate()
+makeLuaText('watermark', songName .. " | Port By ZeonPlay & Raiden", 0, 2, 700); -- You can edit the created by Prisma Text just dont change anything else
+    setTextSize('watermark', 20);
+    addLuaText('watermark');
+end
